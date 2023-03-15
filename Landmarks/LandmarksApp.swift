b83f689b0939c7de707be1f4c8fc285f45cf6585
kubracennet <kubracennetyavasoglu@gmail.com>
2023-03-16 01:40:12 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Kübra Cennet Yavaşoğlu on 16.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
