327d5b1a0947217d0ac4348a85de73e595fb32da
kubracennet <kubracennetyavasoglu@gmail.com>
2023-03-17 02:07:02 +0300

    MapView , iPhone simulator change . Generel view

--- Landmarks/Views/CircleImage.swift
+++ Landmarks/Views/CircleImage.swift
@@ -8,9 +8,11 @@
 import SwiftUI
 
 struct CircleImage: View {
+    var image: Image
+    
     var body: some View {
-        Image("turtlerock")
-           .clipShape(Circle())
+        image
+            .clipShape(Circle())
 //fotoğrafı daire şekline getirmek için.
            .overlay {
                Circle().stroke(.white, lineWidth: 4)
@@ -22,6 +24,6 @@ struct CircleImage: View {
 
 struct CircleImage_Previews: PreviewProvider {
     static var previews: some View {
-        CircleImage()
+        CircleImage(image: Image("turtlerock"))
     }
 }
